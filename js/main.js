50c5e395e102652f68bdbdfd9656ce8d54bdf675
Simon Busch <simon.busch@live.fr>
2020-09-30 08:39:08 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,11 +9,10 @@ const DOMstrings = {
     button: '.button__popup'
 };
 
-
-//Calculation controller
-const minPrice = 4.5;
-const needleCost = 20;
-const otherCosts = 25; 
+const minPrice, needleCost,otherCosts;
+minPrice = 4.5;
+needleCost = 20;
+otherCosts = 25; 
 
 
 function getHeight() {
@@ -31,33 +30,28 @@ function getRadio () {
     if (document.querySelector(DOMstrings.radioBlackAndGrey).checked) {
         let blackAndGrey = 1;
         return blackAndGrey;
-        //console.log(blackAndGrey);
         
     } else if (document.querySelector(DOMstrings.radioColor).checked) {
         let color = 1.5;
         return color;
-        //console.log(color)
+
     } else if (document.querySelector(DOMstrings.radioShades).checked) {
         let shades = 1.5;
         return shades;
-        //console.log(shades);
+
     } else if (document.querySelector(DOMstrings.radioTraditionnal).checked) {
         let traditionnal = 2;
         return traditionnal;
-        //console.log(traditionnal);
     }
 };
 
 
 function calcPrice () {
     let height = (minPrice*1.05) *  (getHeight())/10;
-    //console.log(height);
     let width = (minPrice*1.05) * (getWidth()) /10;
-    //console.log(width);
     let radio = getRadio();
 
     let finalPrice = Math.floor((height*width)*(minPrice*radio) + needleCost + otherCosts);
-    //console.log(finalPrice);
     return finalPrice;
 };
 
@@ -74,7 +68,6 @@ function clearFields () {
 };
 
 function clearPrice () {
-    //document.querySelector(DOMstrings.price).parentElement.removeChild(DOMstrings.price);
     var priceField = document.querySelector('.popup__price');
     priceField.removeChild(priceField.lastElementChild);
     console.log('banana');
@@ -94,16 +87,11 @@ function UIupdate() {
 
 let controller = (function () {
 
-
     document.querySelector('.btn--UI').addEventListener('click', UIupdate);
-
-    
-
 
     document.addEventListener('keypress', function (event) {
 
         if (event.keyCode === 13 || event.which === 13) {
-            //console.log('banane');
             UIupdate();
         }
     })
